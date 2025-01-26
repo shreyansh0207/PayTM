28d1e27425360fe0ec97d23983cfb1407d6e035a
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-01-26 12:38:48 +0530

    Backend section

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
 
+const app = express();
 
+
+app.get("/",function(req,res){
+
+})
+
+app.post("/",function(req,res){
+
+})
+
+app.listen(3000,function(){
+    console.log("Running");
+});
