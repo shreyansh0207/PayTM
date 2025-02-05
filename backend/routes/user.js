6ff9571bdd35c7e1977d62ab9ac801507fcf5720
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-02-06 00:01:19 +0530

    BACKEND SECTION

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const router = express.Router();
+
 const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const { User } = require("../db");
+const JWT_SECRET = require("../config");
+const router = express.Router();
 
 const signupSchema = zod.object({
     username : zod.string(),
@@ -11,8 +15,38 @@ const signupSchema = zod.object({
 
 
 
-router.post("/signup" , (req,res) =>{
+router.post("/signup" , async(req,res) =>{
   const body = req.body;
+  const {success} = signupSchema.safeParse(req.body);
+  if(!success){
+    return res.json ({
+        message : "Email already taken / Incorrect inputs"
+    })
+  }
+
+  const user = User.findone({  //check wheater user valid
+    username : body.username
+  })
+
+
+  if(user._id){
+    return res.json({
+        message : "Email already taken / Incorrect inputs"
+    })
+  }
+  const dbUser = await User.create(body);
+  const token  = jwt.sign({
+    userId : dbUser._id
+  }  , JWT_SECRET);
+  res.json({
+
+    message : "User created successfully",
+    token : token
+  })
+
+
+
+
 })
 
 module.exports = router;
