218f680230de1777181104486936dbe62c16cb4a
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-02-05 23:43:00 +0530

    BACKEND SECTION

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const zod = require("zod");
+
+const signupSchema = zod.object({
+    username : zod.string(),
+    password : zod.string(),
+    firstname : zod.string(),
+    password :zod.string(),
+})
+
+
+
+router.post("/signup" , (req,res) =>{
+  const body = req.body;
+})
 
 module.exports = router;
 // const app = express();
